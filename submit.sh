35f5e5f1dbe2ab7f3bc6c6f813da32c5f76b7e48
Freya Blekman <freya.blekman@cern.ch>
2015-08-03 22:17:43 +0200

    update

--- submit.sh
+++ submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-njobs=5
+njobs=3
 counter=0
 ii=0
 samplename=$2
@@ -11,13 +11,18 @@ for infile in `cat $1`
 do 
     outname=$samplename"_"$counter
     rootfilename=$outname"_tree.root"
+    rootfilenamedisp="disp_"$outname"_tree.root"
     fileoutname="xmlfiles/scripted_"$outname".xml"
+    fileoutnamedisp="xmlfiles/scripted_displaced_"$outname".xml"
 #    echo $samplename " " $counter " "$outname" " $infile
-    cat dummyconfig.xml | sed -e s%FILENAME%$infile%g -e s%NAME%$outname%g > $fileoutname
+    cat dummyconfig.xml | sed -e s%FILENAME%$infile%g -e s%NAME%$outname%g > $fileoutname                                                
+    cat dummyconfig.xml | sed -e s%FILENAME%$infile%g -e s%NAME%$outnamedisp%g > $fileoutnamedisp
+
     echo $fileoutname
     
 #    { ./Ntupler $fileoutname >& $sampledir/log_$outname ; mv $rootfilename $sampledir/$rootfilename } &
     { { ./Ntupler $fileoutname ; mv $rootfilename $sampledir/$rootfilename; echo $infile >> done_$sampledir; } & };
+    { { ./NtuplerDisplaced $fileoutnamedisp ; mv $rootfilenamedisp $sampledir/$rootfilenamedisp; echo $infile >> done_$sampledir; } & };
     if [ $ii = $njobs ]; then
 	echo "now waiting for:" $samplename " " $counter " "$outname" " $infile
 	waitabit=$!
