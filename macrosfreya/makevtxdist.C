c9da88971aa99c36bb93e9a7c413d49a6344d5cc
Freya Blekman <freya.blekman@cern.ch>
2015-10-23 17:36:53 +0200

    new version with slightly different configuration

--- macrosfreya/makevtxdist.C
+++ macrosfreya/makevtxdist.C
@@ -1,20 +1,27 @@
 {
-  TH1F *pileup = new TH1F("pileup","pileup",100,0,100);
-  pileup->Sumw2();
-
-  TChain *ch = new TChain("startevents","startevents");
-  ch->Add("DoubleEG/*.root");
-  ch->Add("DoubleMuon/*.root");
-  ch->Add("MuonEG/*.root");
-  ch->Add("SingleElectron/*.root");
-  ch->Add("SingleMuon/*.root");
-  std::cout << "using " << ch->GetEntries() << " events to make pu histogram:" << std::endl;
-  ch->Draw("nvtx>>pileup");
-    
-  double scaler = 1.;
-  if(ch->GetEntries()>0)
-    scaler /= ch->GetEntries();
-  pileup->Scale(scaler);
-  pileup->SaveAs("pileup_data_Summer15.root");
-
+	TH1F *pileup = new TH1F("pileup","pileup",100,0,100);
+	pileup->Sumw2();
+	TH1F *nvtxdist = (TH1F*) pileup->Clone("nvtxdist");
+	
+	TChain *ch = new TChain("startevents","startevents");
+//	ch->Add("../SingleMuon-Run2015C-23Sep2015-v1/*.root");
+	ch->Add("../SingleMuon-Run2015D-PromptReco-v3/*.root");
+	ch->Add("../SingleMuon-Run2015D-PromptReco-v4/*.root");
+	
+	ch->Add("../DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/*.root");
+	ch->Add("../TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/*.root");
+	
+	std::cout << "using " << ch->GetEntries() << " events to make pu histogram:" << std::endl;
+	ch->Draw("npu>>pileup");
+	ch->Draw("nvtx>>nvtxdist");
+//	ch->Print();
+	
+	double scaler = 1.;
+	if(ch->GetEntries()>0)
+		scaler /= ch->GetEntries();
+		
+	pileup->Scale(scaler);
+//	pileup->SaveAs("pileup_MC_RunIISpring15DR74-Asympt25ns.root");
+		pileup->SaveAs("nominal.root");
+		
 }
