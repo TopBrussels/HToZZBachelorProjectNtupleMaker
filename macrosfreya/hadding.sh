3190f490a47c100ddd93f0ab74c94aec9046fb24
Freya Blekman <freya.blekman@cern.ch>
2015-11-17 22:49:41 +0100

    added some more functionality

--- macrosfreya/hadding.sh
+++ macrosfreya/hadding.sh
@@ -1,1 +1,2 @@
-ls ../ | grep _Ntupler | awk -F"_Ntupler" '{print "hadd ntuples/"$1".root ../"$1"_Ntupler/*.root"}'
+rm -rf ntuples/*.root
+ls ../ | grep _Ntupler | awk -F"_Ntupler" '{print "hadd ntuples/"$1".root ../"$1"_Ntupler/*.root"}' | sh
