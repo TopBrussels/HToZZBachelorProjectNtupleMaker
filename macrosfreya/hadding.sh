9ad2083c1492e9feadecb37ae66ea2e5f22f8532
Freya Blekman <freya.blekman@cern.ch>
2016-02-04 13:29:27 +0100

    slight change, now remove output merged file only during hadd command script

--- macrosfreya/hadding.sh
+++ macrosfreya/hadding.sh
@@ -1,3 +1,3 @@
-rm -rf ntuples/*.root
-ls ../ | grep _Ntupler | awk -F"_Ntupler" '{print "hadd ntuples/"$1".root ../"$1"_Ntupler/*.root"}' | grep Run2015 | sh
-ls ../ | grep _Ntupler | awk -F"_Ntupler" '{print "hadd ntuples/"$1".root ../"$1"_Ntupler/*.root &"}' | grep 13TeV | sh
+#rm -rf ntuples/*.root
+ls ../ | grep _Ntupler | awk -F"_Ntupler" '{print "rm -rf ntuples/"$1".root; hadd ntuples/"$1".root ../"$1"_Ntupler/*.root"}' | grep Run2015 | sh
+ls ../ | grep _Ntupler | awk -F"_Ntupler" '{print "rm -rf ntuples/"$1".root; hadd ntuples/"$1".root ../"$1"_Ntupler/*.root &"}' | grep 13TeV | sh
